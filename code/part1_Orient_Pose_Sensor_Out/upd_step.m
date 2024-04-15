e80e72b497d1980d1edb9ade1ad4c89b25218ebb
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-04-14 22:57:48 -0400

    Part 1 : Slight disparity in UKF and Vicon, tunining required

--- code/part1_Orient_Pose_Sensor_Out/upd_step.m
+++ code/part1_Orient_Pose_Sensor_Out/upd_step.m
@@ -5,5 +5,17 @@ function [uCurr,covar_curr] = upd_step(z_t,covarEst,uEst)
     %covarEst - estimated covar of the  state
     %uEst - estimated mean of the state
     
+% MY IMPLEMENTATION START -------------------------------------------------
+    % Based on analysis, initialise the matrix C as shown below
+    C = horzcat(eye(6), zeros(6,9));
+    % Tuned value for R
+    R = eye(6) * 0.00001;
+    % Kalman Gain
+    K = (covarEst * C') / ((C * covarEst * C') + R);
+    % Updated Covariance matrix
+    covar_curr = covarEst - (K * C * covarEst);
+    % Updated State Matrix
+    uCurr = uEst + (K * (z_t - (C*uEst)));
+% MY IMPLEMENTATION END ---------------------------------------------------
 end
 
